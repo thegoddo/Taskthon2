0bfa2a82896c548ef00d6d80a2b9cac9529b1c9c
theGoddo <biswajitshaw220@gmail.com>
2026-05-19 13:56:02 +0530

    new functions

--- database/db_manager.py
+++ database/db_manager.py
@@ -3,12 +3,15 @@ from datetime import datetime
 
 DB_NAME = "tasks.db"
 
+
 def get_connection():
     """Established and returns a connection to the SQLite database."""
     conn = sqlite3.connect(DB_NAME)
 
-    conn.row_factory = sqlite3.Row # Let's us access columns by name(e.g., row['column_name'])
+    # Let's us access columns by name(e.g., row['column_name'])
+    conn.row_factory = sqlite3.Row
     return conn
+
 
 def initialize_db():
     """Creates the tasks table if it doesn't already exist."""
@@ -24,3 +27,28 @@ def initialize_db():
                        """)
         conn.commit()
     print("Database initialized successfully.")
+
+
+def add_task(title, description):
+    """Inserts a new task into the database."""
+
+    current_time = datetime.now().isoformat()  # Get current time in ISO format
+    with get_connection() as conn:
+        cursor = conn.curson()
+        cursor.execute(
+            """INSERT INTO tasks (title, description, statu, created_at) VALUES (?, ?, ?, ?)""", 
+            (title, description, "pending", current_time))
+        conn.commit()
+        return cursor.lastrowid  # Return the ID of the newly created task
+
+
+def get_all_tasks():
+    """Retrieves all tasks from the database."""
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM tasks ORDER BY id DESC")
+        rows = cursor.fetchall()
+        
+        return [dict(row) for row in rows]  # Convert rows to list of dictionaries
+
+
