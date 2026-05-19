43d22a3e75fbd66e56bc7d97825d51ef86b64ff9
theGoddo <biswajitshaw220@gmail.com>
2026-05-19 13:47:55 +0530

    db initialize function

--- database/db_manager.py
+++ database/db_manager.py
@@ -10,3 +10,17 @@ def get_connection():
     conn.row_factory = sqlite3.Row # Let's us access columns by name(e.g., row['column_name'])
     return conn
 
+def initialize_db():
+    """Creates the tasks table if it doesn't already exist."""
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS tasks (
+                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       title TEXT NOT NULL,
+                       description TEXT,
+                       status TEXT DEFAULT 'pending',
+                       created_at TEXT NOT NULL)
+                       """)
+        conn.commit()
+    print("Database initialized successfully.")
